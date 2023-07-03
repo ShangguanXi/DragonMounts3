e98b7b0783d0fe04485ecd5eb5c8709536d25d7c
上官汐 <78731793+ShangguanXi@users.noreply.github.com>
2023-07-03 16:28:23 +0800

    GlowRenderer
    GlowRenderer

--- src/main/java/net/dragonmounts/client/renders/AetherDragonEntityRenderer.java
+++ src/main/java/net/dragonmounts/client/renders/AetherDragonEntityRenderer.java
@@ -6,14 +6,17 @@ import net.dragonmounts.entities.dragonEggs.AetherDragonEggEntity;
 import net.dragonmounts.entities.dragons.AetherDragonEntity;
 import net.minecraft.client.render.entity.EntityRenderDispatcher;
 import net.minecraft.client.render.entity.MobEntityRenderer;
+import net.minecraft.client.render.entity.feature.EndermanEyesFeatureRenderer;
 import net.minecraft.util.Identifier;
 
 public class AetherDragonEntityRenderer extends MobEntityRenderer<AetherDragonEntity, DragonDefaultModel<AetherDragonEntity>> {
 
-    private static final Identifier TEXTURE = new Identifier("textures/entity/aether_dragon/aether_dragon.tga");
+    private static final Identifier TEXTURE = new Identifier("textures/entity/aether_dragon/aether_dragon.png");
 
     public AetherDragonEntityRenderer(EntityRenderDispatcher dispatcher) {
         super(dispatcher, new DragonDefaultModel<>(), 0.5f);
+        //发光渲染 GlowRenderer
+        //this.addFeature(new AetherDragonGlowRenderer<>(this));
     }
 
     @Override
