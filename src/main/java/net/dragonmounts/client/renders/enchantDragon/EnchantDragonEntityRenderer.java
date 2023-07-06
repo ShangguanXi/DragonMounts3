0b61960310c29ed54aa6a0a5b61619455e253d86
BlueDream <demwre@outlook.com>
2023-07-06 19:40:52 +0800

    Make EnchantDragon available.

--- src/main/java/net/dragonmounts/client/renders/enchantDragon/EnchantDragonEntityRenderer.java
+++ src/main/java/net/dragonmounts/client/renders/enchantDragon/EnchantDragonEntityRenderer.java
@@ -2,13 +2,16 @@ package net.dragonmounts.client.renders.enchantDragon;
 
 import net.dragonmounts.client.models.DragonDefaultModel;
 import net.dragonmounts.entities.dragons.AetherDragonEntity;
+import net.dragonmounts.entities.dragons.EnchantDragonEntity;
 import net.minecraft.client.render.entity.EntityRenderDispatcher;
 import net.minecraft.client.render.entity.MobEntityRenderer;
 import net.minecraft.util.Identifier;
 
-public class EnchantDragonEntityRenderer extends MobEntityRenderer<AetherDragonEntity, DragonDefaultModel<AetherDragonEntity>> {
+//public class EnchantDragonEntityRenderer extends MobEntityRenderer<AetherDragonEntity, DragonDefaultModel<AetherDragonEntity>> {
+public class EnchantDragonEntityRenderer extends MobEntityRenderer<EnchantDragonEntity, DragonDefaultModel<EnchantDragonEntity>>{
 
-    private static final Identifier TEXTURE = new Identifier("textures/entity/aether_dragon/aether_dragon.png");
+    //private static final Identifier TEXTURE = new Identifier("textures/entity/aether_dragon/aether_dragon.png");
+    private static final Identifier TEXTURE = new Identifier("textures/entity/enchant_dragon/enchant_dragon.png");
 
     public EnchantDragonEntityRenderer(EntityRenderDispatcher dispatcher) {
         super(dispatcher, new DragonDefaultModel<>(), 0.5f);
@@ -17,7 +20,7 @@ public class EnchantDragonEntityRenderer extends MobEntityRenderer<AetherDragonE
     }
 
     @Override
-    public Identifier getTexture(AetherDragonEntity entity) {
+    public Identifier getTexture(EnchantDragonEntity entity) {
         return TEXTURE;
     }
 }
--- src/main/java/net/dragonmounts/entities/Dragons.java
+++ src/main/java/net/dragonmounts/entities/Dragons.java
@@ -1,7 +1,9 @@
 package net.dragonmounts.entities;
 
 import net.dragonmounts.client.renders.aetherDragon.AetherDragonEntityRenderer;
+import net.dragonmounts.client.renders.enchantDragon.EnchantDragonEntityRenderer;
 import net.dragonmounts.entities.dragons.AetherDragonEntity;
+import net.dragonmounts.entities.dragons.EnchantDragonEntity;
 import net.fabricmc.fabric.api.client.rendereregistry.v1.EntityRendererRegistry;
 import net.fabricmc.fabric.api.object.builder.v1.entity.FabricDefaultAttributeRegistry;
 import net.fabricmc.fabric.api.object.builder.v1.entity.FabricEntityTypeBuilder;
@@ -18,11 +20,19 @@ public class Dragons {
                     FabricEntityTypeBuilder.create(SpawnGroup.MISC, AetherDragonEntity::new)
                             .dimensions(EntityDimensions.fixed(0.875F, 0.875F))
                             .build());
+    public static final EntityType<EnchantDragonEntity> EnchantDragon =
+            Registry.register(Registry.ENTITY_TYPE,
+                    new Identifier("dragonmounts", "enchant_dragon"),
+                    FabricEntityTypeBuilder.create(SpawnGroup.MISC, EnchantDragonEntity::new)
+                            .dimensions(EntityDimensions.fixed(0.875F, 0.875F))
+                            .build());
     public static void registerEntityRenderer() {
         EntityRendererRegistry.INSTANCE.register(AetherDragon, (dispatcher, context) ->
                 new AetherDragonEntityRenderer(dispatcher));
+        EntityRendererRegistry.INSTANCE.register(EnchantDragon, (dis, context) -> new EnchantDragonEntityRenderer(dis));
     }
     public static void registerEntityAttributes() {
         FabricDefaultAttributeRegistry.register(AetherDragon, AetherDragonEntity.createAetherDragonAttributes());
+        FabricDefaultAttributeRegistry.register(EnchantDragon, EnchantDragonEntity.createEnchantDragonAttributes());
     }
 }
